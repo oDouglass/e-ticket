8079fc70fbd50ffc08d08f59e0672d8482c02437
oDouglass <douglasluiz_855@hotmail.com>
2024-05-04 14:17:57 -0300

    Inicial

---
+++ src/app/sobre-nos/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const SobreNosPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Sobre Nós</h1>
+        </div>
+    )
+}
+
+export default SobreNosPage;
